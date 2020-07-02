72e9826ef3e41cf8c9735686d273fdf6173575b8
jojo3420 <jjjhhhvvv@gmail.com>
2020-07-02 14:53:26 +0900

    3. React.lazy, Suspense 조합을 이용한 동적 컴포넌트 import

--- src/App.js
+++ src/App.js
@@ -1,20 +1,16 @@
-import React  from 'react';
+import React, { useState, Suspense }  from 'react';
 import './App.css';
 
 
-// 주의할점. 클래스 컴포넌트만 불러올 수 있음.
-class App extends React.Component {
+function App() {
+  const [visible, setVisible] = useState(false);
 
-  state = {
-    SplitMe: null,
-  };
-
-  handleClick = async () => {
-    const loadedModule = await import('./components/SplitMe');
-    this.setState({ SplitMe: loadedModule.default})
+  const handleClick =  () => {
+    setVisible(!visible)
   }
-  render() {
-    const { SplitMe } = this.state;
+
+  // 주의: lazy()는 리액트 컴포넌트 만 불러올 수 있음!
+  const SplitMe = React.lazy(() => import('./components/SplitMe'));
 
     return (
       <div className="App">
@@ -23,12 +19,13 @@ class App extends React.Component {
           일반 자바스크립트 함수 동적으로 import 하기
         </h3>
         <main>
-          <button onClick={this.handleClick}>불러오기</button>
-          {SplitMe && <SplitMe/>}
+          <button onClick={handleClick}>show/hide</button>
+          <Suspense fallback={<div>Loading...</div>}>
+            {visible && <SplitMe /> }
+          </Suspense>
         </main>
       </div>
     );
-  }
 }
 
 export default App;
