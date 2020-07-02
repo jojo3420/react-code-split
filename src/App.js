274cb5418671f9cea20d6c5d008ecf2ad9eb9348
jojo3420 <jjjhhhvvv@gmail.com>
2020-07-02 14:42:39 +0900

    2. 코드 스플릿트 를 이용하여 컴포넌트 동적 불러오기  - 클래스 컴포넌트

--- src/App.js
+++ src/App.js
@@ -1,26 +1,34 @@
-import React from 'react';
+import React  from 'react';
 import './App.css';
 
 
-function App() {
+// 주의할점. 클래스 컴포넌트만 불러올 수 있음.
+class App extends React.Component {
 
-  const handleClick = () => {
-    import('./util/greenting')
-      .then(result => result.default());
+  state = {
+    SplitMe: null,
+  };
+
+  handleClick = async () => {
+    const loadedModule = await import('./components/SplitMe');
+    this.setState({ SplitMe: loadedModule.default})
   }
+  render() {
+    const { SplitMe } = this.state;
 
-  return (
-    <div className="App">
-      <h1>코드 스플리팅</h1>
-      <h3>
-        일반 자바스크립트 함수 동적으로 import 하기
-      </h3>
-      <main>
-        <button onClick={handleClick}>불러오기
-        </button>
-      </main>
-    </div>
-  );
+    return (
+      <div className="App">
+        <h1>코드 스플리팅</h1>
+        <h3>
+          일반 자바스크립트 함수 동적으로 import 하기
+        </h3>
+        <main>
+          <button onClick={this.handleClick}>불러오기</button>
+          {SplitMe && <SplitMe/>}
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
